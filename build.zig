df53874a196e4a754b19ce5d06ebc98df562304c
valentinnamleev <vlntnnmlv@gmail.com>
2026-01-18 00:06:33 +0100

    Updatr build.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("asset", .{
+    _ = b.addModule("fey_asset", .{
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
