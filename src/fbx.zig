a116d03d04a2b45bd91504f75a8267d5fa8cab26
valentinnamleev <vlntnnmlv@gmail.com>
2026-01-18 01:39:55 +0100

    Fix UV's path nodes order

--- src/fbx.zig
+++ src/fbx.zig
@@ -424,7 +424,7 @@ pub const FBXFile = struct {
     }
 
     pub fn uvs(self: Self, allocator: std.mem.Allocator) ![]zlm.Vec2 {
-        const data = self.children.get("Objects").?.children.get("Geometry").?.children.get("UV").?.children.get("LayerElementUV").?.properties.items[0].data.ArrayDouble;
+        const data = self.children.get("Objects").?.children.get("Geometry").?.children.get("LayerElementUV").?.children.get("UV").?.properties.items[0].data.ArrayDouble;
         const num_uvs = @divExact(data.len, 2);
         var result: []zlm.Vec2 = try allocator.alloc(zlm.Vec2, num_uvs);
         for (0..num_uvs) |i| {
